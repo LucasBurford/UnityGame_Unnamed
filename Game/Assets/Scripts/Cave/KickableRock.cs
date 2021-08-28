b96db0054a7927aa7b98bb887b21e2ce3c3554e1
Lucas-Burford <lucasburford@live.co.uk>
2021-08-28 16:29:50 +0100

    Added interactive rocks to game
    Placed 3 rocks in Cave

---
+++ Game/Assets/Scripts/Cave/KickableRock.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KickableRock : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        FindObjectOfType<AudioManager>().Play("RockHit1");
+    }
+}
