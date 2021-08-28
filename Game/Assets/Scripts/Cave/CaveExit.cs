5a49370a26ea50476e97bd15f0184dc01a28dcee
Lucas-Burford <lucasburford@live.co.uk>
2021-08-28 18:19:26 +0100

    Rearranged asset folders

--- Game/Assets/Scripts/Cave/CaveExit.cs
+++ Game/Assets/Scripts/Cave/CaveExit.cs
@@ -8,6 +8,27 @@ public class CaveExit : MonoBehaviour
     public GameManager gameManager;
     public PlayerMovement player;
 
+    private int x;
+
+    private void Update()
+    {
+        // Generate random numbers to play random drip sounds
+        x = Random.Range(0, 100);
+
+        switch (x)
+        {
+            //case 1:
+            //    {
+            //        PlayRandomSound("")
+            //    }
+        }
+    }
+
+    public void PlayRandomSound(string clip)
+    {
+        FindObjectOfType<AudioManager>().Play(clip);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name == "Player_Knight")
