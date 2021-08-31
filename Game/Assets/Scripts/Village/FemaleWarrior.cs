40caa26955afabb6f523c01741fdd036d8549f04
Lucas-Burford <lucasburford@live.co.uk>
2021-08-31 13:02:43 +0100

    Added Female Warrior to village, added Bolt to project

---
+++ Game/Assets/Scripts/Village/FemaleWarrior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FemaleWarrior : MonoBehaviour
+{
+    public DialogueTrigger dialogueTrigger;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player_Knight")
+        {
+            dialogueTrigger.TriggerDialogue();
+
+            FindObjectOfType<AudioManager>().Play("WizardSpeaking");
+        }
+    }
+}
