94cd8b4b809f668ef1eccd3405536e0220bfdff3
Lucas-Burford <lucasburford@live.co.uk>
2021-09-01 13:04:35 +0100

    Added entry trigger to Dark Forest, added Dark Skeleton enemy

---
+++ Game/Assets/Scripts/Enemies/DarkSkeleton.cs
@@ -0,0 +1,229 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkSkeleton : MonoBehaviour
+{
+    #region References
+    public GameManager gameManager;
+    public Animator animator;
+
+    public Transform attackPoint;
+
+    public Material material;
+    #endregion
+
+    #region Gameplay
+    [SerializeField]
+    private float maxHealth;
+
+    [SerializeField]
+    private float currentHealth;
+
+    [SerializeField]
+    private float attackRange;
+
+    [SerializeField]
+    private float maxDamage;
+
+    [SerializeField]
+    private float damage;
+
+    [SerializeField]
+    private float fade;
+
+    [SerializeField]
+    private float fadeFactor;
+
+    [SerializeField]
+    private bool canAttack;
+
+    [SerializeField]
+    private bool isDissolving;
+
+    public bool isBeingBlocked;
+
+    private bool hasPlayed;
+
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+
+        maxHealth = 200;
+        currentHealth = maxHealth;
+
+        fade = 1;
+
+        damage = maxDamage;
+
+        canAttack = true;
+
+        material.SetFloat("_Fade", fade);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+
+        if (isDissolving)
+        {
+            fade -= fadeFactor;
+
+            if (fade <= 0)
+            {
+                fade = 0;
+                isDissolving = false;
+            }
+
+            material.SetFloat("_Fade", fade);
+        }
+
+        if (Random.Range(0, 1000) < 5)
+        {
+            PlaySkeletonSound("SkeletonIdle");
+        }
+    }
+
+    private void AttackPlayer()
+    {
+        // Get collided objects by casting a circle
+        Collider2D[] objects = Physics2D.OverlapCircleAll(attackPoint.position, attackRange);
+
+        foreach (Collider2D col in objects)
+        {
+            // Handle collision with player
+            if (col.gameObject.tag == "Player")
+            {
+                Debug.Log("Hit Player");
+
+                // Play attack animation
+                animator.SetBool("IsAttacking", true);
+
+                //-- Take damage moved to ResetAttackAnimation to match animation timings --\\
+
+                // Set canAttack to false
+                canAttack = false;
+
+                // Reset animation back to idle
+                StartCoroutine(ResetAttackAnimation(0.7f));
+
+                // Set canAttack back to true
+                StartCoroutine(ResetAttackBool(2));
+            }
+
+            // Handle shield block audio
+            if (col.gameObject.name == "PlayerShield")
+            {
+                StartCoroutine(WaitToPlayShieldSound(0.7f));
+            }
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        // Deduct passed in damage from current health
+        currentHealth -= damage;
+    }
+
+    private void Die()
+    {
+        Debug.Log("Skeleton died");
+
+        // Set canAttack to false so he doesn't attack while dissolving
+        canAttack = false;
+
+        if (!hasPlayed)
+        {
+            PlaySkeletonSound("SkeletonDeath");
+        }
+
+        hasPlayed = true;
+
+        // Play dissolve animation
+        isDissolving = true;
+
+        // Wait for time to destroy object
+        StartCoroutine(WaitToDestroy(3));
+    }
+
+    private void PlaySkeletonSound(string name)
+    {
+        FindObjectOfType<AudioManager>().Play(name);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player_Knight")
+        {
+            if (canAttack)
+            {
+                AttackPlayer();
+            }
+            else
+            {
+                Debug.Log("Can't attack yet!");
+            }
+        }
+    }
+
+    public void ChangeAttackDamage(float amount)
+    {
+        damage = amount;
+
+        StartCoroutine(ResetAttackDamage(2));
+    }
+
+    IEnumerator ResetAttackDamage(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        damage = maxDamage;
+    }
+
+    IEnumerator ResetAttackAnimation(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        // Damge the player
+        gameManager.TakeDamage(damage);
+
+        // Reset animation back to idle
+        animator.SetBool("IsAttacking", false);
+    }
+
+    IEnumerator ResetAttackBool(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        canAttack = true;
+    }
+
+    IEnumerator WaitToDestroy(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        Destroy(gameObject);
+    }
+
+    IEnumerator WaitToPlayShieldSound (float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        FindObjectOfType<AudioManager>().Play("ShieldHit");
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
---
+++ Game/Assets/Scripts/Village/DarkForestEntry.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DarkForestEntry : MonoBehaviour
+{
+    public GameObject darkForestArea;
+
+    public TMP_Text darkForestEntryText;
+
+    public bool isActive;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player_Knight")
+        {
+            if (!isActive)
+            {
+                // Enable area
+                darkForestArea.SetActive(true);
+
+                // Set isActive to true
+                isActive = true;
+
+                // Display text
+                darkForestEntryText.GetComponent<TextMeshProUGUI>().enabled = true;
+                darkForestEntryText.text = "Dark Forest";
+
+                // Wait to remove text
+                StartCoroutine(Wait(2));
+            }
+            else
+            {
+                // Display text
+                darkForestEntryText.GetComponent<TextMeshProUGUI>().enabled = true;
+                darkForestEntryText.text = "Highpoint Village";
+
+                // Wait to remove text
+                StartCoroutine(Wait(2));
+
+                // Wait to disable areas
+                StartCoroutine(Wait2(3));
+            }
+        }
+    }
+
+    IEnumerator Wait(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        darkForestEntryText.GetComponent<TextMeshProUGUI>().enabled = false;
+    }
+
+    IEnumerator Wait2(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        // Disable area
+        darkForestArea.SetActive(false);
+
+        // Set isActive back to false
+        isActive = false;
+    }
+}
