c74bd3f5f216bf9336ecd167a6e0c83a2b2fe1ee
Lucas-Burford <lucasburford@live.co.uk>
2021-11-21 13:36:11 +0000

    Added audio to Castle

---
+++ Game/Assets/Scripts/Castle/Castle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Castle : MonoBehaviour
+{
+    #region Members
+    [Header("References")]
+    public PlayerMovement playerMovement;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region Unity Methods
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player_Knight")
+        {
+            playerMovement.surface = PlayerMovement.Surface.stone;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player_Knight")
+        {
+            playerMovement.surface = PlayerMovement.Surface.grass;
+        }
+    }
+    #endregion
+}
